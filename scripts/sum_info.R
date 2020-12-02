1c0150afc0eafcc530f8c0842c6cb3fcff8b3bec
wrileyb <wrilsb@uw.edu>
2020-12-02 13:59:25 -0800

    fixed sum_info, updated library of index file

--- scripts/sum_info.R
+++ scripts/sum_info.R
@@ -1,8 +1,1 @@
-library(dplyr)
 
-pop_race <- read.csv("Project-Midpoint/data/demographics_by_race.csv")
-sum_info <- function(d_frame) {
-  info <- list()
-  info$length <- length(d_frame)
-  info$max <- max(d_frame$Biden Votes)
-}
